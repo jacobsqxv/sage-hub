70d360fe48cec6d96a442acdfdf540e01d40ff82
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-22 19:48:32 +0000

    build: add base application files

---
+++ src/test/java/dev/aries/sagehub/SageHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aries.sagehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SageHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
