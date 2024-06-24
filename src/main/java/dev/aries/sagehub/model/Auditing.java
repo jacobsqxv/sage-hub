17e3d58af59b288a58f75f1ba8fa70b30c22beef
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-24 20:09:01 +0000

    feat: add auditing for entities

---
+++ src/main/java/dev/aries/sagehub/model/Auditing.java
@@ -0,0 +1,35 @@
+package dev.aries.sagehub.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditing {
+
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(insertable = false)
+	private LocalDateTime updatedAt;
+
+}
