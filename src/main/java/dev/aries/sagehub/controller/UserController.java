cd42d36463ad04464496c35bbe601b6c980785e4
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-25 16:48:19 +0000

    feat: add endpoint to update contact info and emergency contact

--- src/main/java/dev/aries/sagehub/controller/UserController.java
+++ src/main/java/dev/aries/sagehub/controller/UserController.java
@@ -1,10 +1,13 @@
 package dev.aries.sagehub.controller;
 
+import dev.aries.sagehub.dto.request.ContactInfoRequest;
+import dev.aries.sagehub.dto.request.EmergencyContactRequest;
 import dev.aries.sagehub.dto.request.PasswordChangeRequest;
 import dev.aries.sagehub.dto.response.BasicUserResponse;
 import dev.aries.sagehub.dto.response.ContactInfoResponse;
 import dev.aries.sagehub.dto.response.EmergencyContactResponse;
 import dev.aries.sagehub.service.userservice.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +29,7 @@ public class UserController {
 	@PostMapping("{user-id}/change-password")
 	public ResponseEntity<BasicUserResponse> changePassword(
 			@PathVariable("user-id") Long id,
-			@RequestBody PasswordChangeRequest request) {
+			@RequestBody @Valid PasswordChangeRequest request) {
 		return ResponseEntity.ok(this.userService.changePassword(id, request));
 	}
 
@@ -34,9 +38,23 @@ public class UserController {
 		return ResponseEntity.ok(this.userService.getContactInfo(id));
 	}
 
+	@PutMapping("{user-id}/contact-info")
+	public ResponseEntity<ContactInfoResponse> updateContactInfo(
+			@PathVariable("user-id") Long id,
+			@RequestBody @Valid ContactInfoRequest request) {
+		return ResponseEntity.ok(this.userService.updateContactInfo(id, request));
+	}
+
 	@GetMapping("{user-id}/emergency-contact")
 	public ResponseEntity<EmergencyContactResponse> getEmergencyContact(
 			@PathVariable("user-id") Long id) {
 		return ResponseEntity.ok(this.userService.getEmergencyContact(id));
+	}
+
+	@PutMapping("{user-id}/emergency-contact")
+	public ResponseEntity<EmergencyContactResponse> updateEmergencyContact(
+			@PathVariable("user-id") Long id,
+			@RequestBody @Valid EmergencyContactRequest request) {
+		return ResponseEntity.ok(this.userService.updateEmergencyContact(id, request));
 	}
 }
