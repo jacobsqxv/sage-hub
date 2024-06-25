6966187e66fb226c8409b2cbd800ce0f5dd70b1c
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-25 16:35:26 +0000

    refactor(dto): add validation to request dtos

--- src/main/java/dev/aries/sagehub/dto/request/PasswordChangeRequest.java
+++ src/main/java/dev/aries/sagehub/dto/request/PasswordChangeRequest.java
@@ -1,7 +1,15 @@
 package dev.aries.sagehub.dto.request;
 
+import dev.aries.sagehub.constant.Patterns;
+import dev.aries.sagehub.constant.ValidationMessage;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public record PasswordChangeRequest(
+		@NotEmpty(message = ValidationMessage.NOT_NULL)
 		String oldPassword,
+		@NotEmpty(message = ValidationMessage.NOT_NULL)
+		@Pattern(regexp = Patterns.PASSWORD, message = ValidationMessage.PASSWORD)
 		String newPassword
 ) {
 }
--- src/main/java/dev/aries/sagehub/dto/request/PasswordResetRequest.java
+++ src/main/java/dev/aries/sagehub/dto/request/PasswordResetRequest.java
@@ -1,8 +1,17 @@
 package dev.aries.sagehub.dto.request;
 
+import dev.aries.sagehub.constant.Patterns;
+import dev.aries.sagehub.constant.ValidationMessage;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public record PasswordResetRequest(
+		@NotEmpty(message = ValidationMessage.NOT_NULL)
 		String username,
+		@NotEmpty(message = ValidationMessage.NOT_NULL)
+		@Pattern(regexp = Patterns.PASSWORD, message = ValidationMessage.PASSWORD)
 		String password,
+		@NotEmpty(message = ValidationMessage.NOT_NULL)
 		String token
 ) {
 }
