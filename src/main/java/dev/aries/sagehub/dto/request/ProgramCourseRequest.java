6907f13c669bfa302dbca0d29ed7e68d048aecc0
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-29 16:05:11 +0000

    refactor(request): update request objects

--- src/main/java/dev/aries/sagehub/dto/request/ProgramCourseRequest.java
+++ src/main/java/dev/aries/sagehub/dto/request/ProgramCourseRequest.java
@@ -1,20 +1,13 @@
 package dev.aries.sagehub.dto.request;
 
-import java.util.List;
-
-import dev.aries.sagehub.constant.Patterns;
 import dev.aries.sagehub.constant.ValidationMessage;
-import jakarta.validation.constraints.NotEmpty;
+import dev.aries.sagehub.model.AcademicPeriod;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record ProgramCourseRequest(
-		@NotEmpty(message = ValidationMessage.NOT_NULL)
-		@Pattern(regexp = Patterns.NAME, message = ValidationMessage.NAME)
-		String name,
-		String description,
 		@NotNull(message = ValidationMessage.NOT_NULL)
-		Long departmentId,
-		List<@NotNull Long> courseIds
+		Long courseId,
+		AcademicPeriod period,
+		String status
 ) {
 }
--- src/main/java/dev/aries/sagehub/dto/request/ProgramRequest.java
+++ src/main/java/dev/aries/sagehub/dto/request/ProgramRequest.java
@@ -3,6 +3,7 @@ package dev.aries.sagehub.dto.request;
 import dev.aries.sagehub.constant.Patterns;
 import dev.aries.sagehub.constant.ValidationMessage;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record ProgramRequest(
@@ -10,6 +11,8 @@ public record ProgramRequest(
 		@Pattern(regexp = Patterns.NAME, message = ValidationMessage.NAME)
 		String name,
 		String description,
+		@NotNull(message = ValidationMessage.NOT_NULL)
+		Long departmentId,
 		String status
 ) {
 }
