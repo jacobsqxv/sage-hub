b343260e103248508d0f451de6b5964a4773f1d1
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-29 17:34:14 +0000

    build: update Java version to 21

--- src/main/java/dev/aries/sagehub/util/UserUtil.java
+++ src/main/java/dev/aries/sagehub/util/UserUtil.java
@@ -3,6 +3,7 @@ package dev.aries.sagehub.util;
 import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.function.Function;
 
 import dev.aries.sagehub.dto.response.BasicUserResponse;
 import dev.aries.sagehub.enums.Gender;
@@ -33,21 +34,21 @@ import static dev.aries.sagehub.constant.ExceptionConstants.NO_INFO_FOUND;
 @Slf4j
 public class UserUtil {
 
+	private static final String CONTACT = "contact";
+	private static final String EMERGENCY_CONTACT = "emergency contact";
+	private static final String USER = "User";
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 	private final RoleUtil roleUtil;
 	private final AdminRepository adminRepository;
 	private final StaffRepository staffRepository;
 	private final StudentRepository studentRepository;
-	private static final String USER = "User";
-	public static final String CONTACT = "contact";
-	public static final String EMERGENCY_CONTACT = "emergency contact";
 	private final Generators generators;
 
 	public User getUser(String username) {
 		return this.userRepository.findByUsername(username)
-			.orElseThrow(
-					() -> new EntityNotFoundException(String.format(NOT_FOUND, USER)));
+				.orElseThrow(
+						() -> new EntityNotFoundException(String.format(NOT_FOUND, USER)));
 	}
 
 	public User getUser(Long id) {
@@ -58,13 +59,13 @@ public class UserUtil {
 	public User createNewUser(String firstName, String lastName, RoleEnum roleEnum) {
 		String password = this.generators.generatePassword();
 		User user = User.builder()
-			.username(this.generators.generateUsername(firstName, lastName))
-			.hashedPassword(this.passwordEncoder.encode(password))
-			.accountEnabled(true)
-			.role(this.roleUtil.getRole(roleEnum))
-			.failedLoginAttempts(0)
-			.status(Status.ACTIVE)
-			.build();
+				.username(this.generators.generateUsername(firstName, lastName))
+				.hashedPassword(this.passwordEncoder.encode(password))
+				.accountEnabled(true)
+				.role(this.roleUtil.getRole(roleEnum))
+				.failedLoginAttempts(0)
+				.status(Status.ACTIVE)
+				.build();
 		log.info("INFO - Generated password for {} is {}", user.getUsername(), password);
 		return this.userRepository.save(user);
 	}
@@ -88,14 +89,17 @@ public class UserUtil {
 	public BasicUserResponse getBasicInfo(Object userInfo) {
 		if (userInfo instanceof Optional<?> optional && optional.isPresent()) {
 			Object user = optional.get();
-			if (user instanceof Admin admin) {
-				return getUserResponse(admin);
-			}
-			else if (user instanceof Staff staff) {
-				return getUserResponse(staff);
-			}
-			else if (user instanceof Student student) {
-				return getUserResponse(student);
+			switch (user) {
+				case Admin admin -> {
+					return getUserResponse(admin);
+				}
+				case Staff staff -> {
+					return getUserResponse(staff);
+				}
+				case Student student -> {
+					return getUserResponse(student);
+				}
+				default -> throw new IllegalArgumentException(String.format(NOT_FOUND, USER));
 			}
 		}
 		throw new IllegalArgumentException(String.format(NOT_FOUND, USER));
@@ -113,65 +117,69 @@ public class UserUtil {
 					.fullname((String) user.getClass().getMethod("fullName").invoke(user))
 					.username(linkedUser.getUsername())
 					.primaryEmail((String) user.getClass().getMethod("getPrimaryEmail").invoke(user))
-					.secondaryEmail(null)
 					.status(linkedUser.getStatus().getValue())
 					.role(linkedUser.getRole().getName().name());
-			if (user instanceof Student || user instanceof Staff) {
-				Enum<Gender> gender;
-				gender = (Enum<Gender>) user.getClass().getMethod("getGender").invoke(user);
-				ContactInfo linkedContactInfo = (ContactInfo) user.getClass()
-						.getMethod("getContactInfo").invoke(user);
-				response.secondaryEmail(linkedContactInfo.getSecondaryEmail());
-				response.dateOfBirth((LocalDate) user.getClass()
-						.getMethod("getDateOfBirth").invoke(user));
-				response.gender(String.valueOf(gender));
-			}
+
 			if (user instanceof Student) {
-				response.studentId(Optional.ofNullable((Long) user.getClass()
-						.getMethod("getId").invoke(user)));
+				getStudentResponse(user, response);
+			} else if (user instanceof Staff) {
+				getStaffResponse(user, response);
 			}
-			if (user instanceof Staff) {
-				response.staffId(Optional.ofNullable((Long) user.getClass()
-						.getMethod("getId").invoke(user)));
-			}
+
 			return response.build();
-		}
-		catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
+		} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
 			throw new IllegalStateException(ex.getMessage());
 		}
 	}
 
 	public ContactInfo loadContactInfo(Object userInfo) {
-		if (userInfo instanceof Optional<?> optional && optional.isPresent()) {
-			Object user = optional.get();
-			if (user instanceof Admin) {
-				throw new EntityNotFoundException(String.format(NO_INFO_FOUND, CONTACT));
+		return loadUserInfo(userInfo, user -> {
+			try {
+				return (ContactInfo) user.getClass().getMethod("getContactInfo").invoke(user);
+			} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
+				throw new IllegalArgumentException(ex);
 			}
-			else if (user instanceof Staff || user instanceof Student) {
-				try {
-					return (ContactInfo) user.getClass().getMethod("getContactInfo").invoke(user);
-				}
-				catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
-					throw new IllegalStateException(ex.getMessage());
-				}
-			}
-		}
-		throw new IllegalArgumentException(String.format(NOT_FOUND, USER));
+		}, CONTACT);
 	}
 
 	public EmergencyContact loadEmergencyContact(Object userInfo) {
+		return loadUserInfo(userInfo, user -> {
+			try {
+				return (EmergencyContact) user.getClass().getMethod("getContactInfo").invoke(user);
+			} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
+				throw new IllegalArgumentException(ex);
+			}
+		}, EMERGENCY_CONTACT);
+	}
+
+	private <T> void getCommonResponse(T user, BasicUserResponse.BasicUserResponseBuilder response)
+			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Enum<Gender> gender = (Enum<Gender>) user.getClass().getMethod("getGender").invoke(user);
+		ContactInfo linkedContactInfo = (ContactInfo) user.getClass().getMethod("getContactInfo").invoke(user);
+		response.secondaryEmail(linkedContactInfo.getSecondaryEmail());
+		response.dateOfBirth((LocalDate) user.getClass().getMethod("getDateOfBirth").invoke(user));
+		response.gender(String.valueOf(gender));
+	}
+
+	private <T> void getStudentResponse(T user, BasicUserResponse.BasicUserResponseBuilder response)
+			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		getCommonResponse(user, response);
+		response.studentId(Optional.ofNullable((Long) user.getClass().getMethod("getId").invoke(user)));
+	}
+
+	private <T> void getStaffResponse(T user, BasicUserResponse.BasicUserResponseBuilder response)
+			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		getCommonResponse(user, response);
+		response.staffId(Optional.ofNullable((Long) user.getClass().getMethod("getId").invoke(user)));
+	}
+
+	private <T> T loadUserInfo(Object userInfo, Function<Object, T> mapper, String exceptionMessage) {
 		if (userInfo instanceof Optional<?> optional && optional.isPresent()) {
 			Object user = optional.get();
 			if (user instanceof Admin) {
-				throw new EntityNotFoundException(String.format(NO_INFO_FOUND, EMERGENCY_CONTACT));
-			}
-			else if (user instanceof Staff || user instanceof Student) {
-				try {
-					return (EmergencyContact) user.getClass().getMethod("getContactInfo").invoke(user);
-				}
-				catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException ex) {
-					throw new IllegalStateException(ex.getMessage());
-				}
+				throw new EntityNotFoundException(String.format(NO_INFO_FOUND, exceptionMessage));
+			} else if (user instanceof Staff || user instanceof Student) {
+				return mapper.apply(user);
 			}
 		}
 		throw new IllegalArgumentException(String.format(NOT_FOUND, USER));
