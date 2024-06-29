c54773f58d1bf0f3ce6223187492480c5e4bead9
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-29 19:19:14 +0000

    feat: add department specification for search and filter

--- src/main/java/dev/aries/sagehub/repository/DepartmentRepository.java
+++ src/main/java/dev/aries/sagehub/repository/DepartmentRepository.java
@@ -2,10 +2,26 @@ package dev.aries.sagehub.repository;
 
 import dev.aries.sagehub.model.Department;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import static dev.aries.sagehub.specification.DepartmentSpecification.hasCode;
+import static dev.aries.sagehub.specification.DepartmentSpecification.hasName;
+import static dev.aries.sagehub.specification.DepartmentSpecification.hasStatus;
 
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long>, JpaSpecificationExecutor<Department> {
 	boolean existsByCode(String code);
 
 	boolean existsByName(String departmentName);
+
+	default Page<Department> findAll(String name, String code, String status, Pageable page) {
+		return findAll(
+				Specification.where(hasName(name))
+						.and(hasCode(code))
+						.and(hasStatus(status)),
+				page
+		);
+	}
 }
