825b397b9b9924b11753c9a78c8c0e5c5383197a
Jacobs Agyei <agyeijacobs@outlook.com>
2024-06-30 19:58:53 +0000

    refactor: add general specification for common criteria

--- src/main/java/dev/aries/sagehub/repository/DepartmentRepository.java
+++ src/main/java/dev/aries/sagehub/repository/DepartmentRepository.java
@@ -1,15 +1,13 @@
 package dev.aries.sagehub.repository;
 
 import dev.aries.sagehub.model.Department;
+import dev.aries.sagehub.specification.GenericSpecification;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import static dev.aries.sagehub.specification.DepartmentSpecification.hasCode;
-import static dev.aries.sagehub.specification.DepartmentSpecification.hasName;
-import static dev.aries.sagehub.specification.DepartmentSpecification.hasStatus;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long>, JpaSpecificationExecutor<Department> {
 	boolean existsByCode(String code);
@@ -17,10 +15,12 @@ public interface DepartmentRepository extends JpaRepository<Department, Long>, J
 	boolean existsByName(String departmentName);
 
 	default Page<Department> findAll(String name, String code, String status, Pageable page) {
+		GenericSpecification<Department> spec = new GenericSpecification<>();
 		return findAll(
-				Specification.where(hasName(name))
-						.and(hasCode(code))
-						.and(hasStatus(status)),
+				Specification
+						.where(spec.hasName(name))
+						.and(spec.hasCode(code))
+						.and(spec.hasStatus(status)),
 				page
 		);
 	}
--- src/main/java/dev/aries/sagehub/specification/DepartmentSpecification.java
+++ src/main/java/dev/aries/sagehub/specification/DepartmentSpecification.java
@@ -1,26 +1,8 @@
 package dev.aries.sagehub.specification;
 
 import dev.aries.sagehub.constant.ExceptionConstants;
-import dev.aries.sagehub.enums.Status;
-import dev.aries.sagehub.model.Department;
 
-import org.springframework.data.jpa.domain.Specification;
-
-public class DepartmentSpecification {
-	public static Specification<Department> hasName(String name) {
-		return (root, query, criteriaBuilder) -> name == null || name.trim().isEmpty() ? null : criteriaBuilder.like(
-				criteriaBuilder.lower(root.get("name")), "%" + name.trim().toLowerCase() + "%");
-	}
-
-	public static Specification<Department> hasCode(String code) {
-		return (root, query, criteriaBuilder) -> code == null || code.isEmpty() ? null : criteriaBuilder.like(
-				root.get("code"), "%" + code + "%");
-	}
-
-	public static Specification<Department> hasStatus(String status) {
-		return (root, query, criteriaBuilder) -> status == null || status.isEmpty() ? null : criteriaBuilder.equal(
-				root.get("status"), Status.valueOf(status.toUpperCase()));
-	}
+public final class DepartmentSpecification {
 
 	private DepartmentSpecification() {
 		throw new IllegalStateException(ExceptionConstants.UTILITY_CLASS);
