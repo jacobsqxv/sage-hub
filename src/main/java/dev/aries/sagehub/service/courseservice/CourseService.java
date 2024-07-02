c52817ebd8d9ca83555c140e1f4ba7160b16384f
Jacobs Agyei <agyeijacobs@outlook.com>
2024-07-02 14:18:53 +0000

    refactor: update pagination for getting courses

--- src/main/java/dev/aries/sagehub/service/courseservice/CourseService.java
+++ src/main/java/dev/aries/sagehub/service/courseservice/CourseService.java
@@ -5,13 +5,14 @@ import dev.aries.sagehub.dto.response.CourseResponse;
 import dev.aries.sagehub.dto.search.GetCoursesPage;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CourseService {
 	CourseResponse addCourse(CourseRequest request);
 
 	CourseResponse getCourse(Long courseId);
 
-	Page<CourseResponse> getCourses(GetCoursesPage request);
+	Page<CourseResponse> getCourses(GetCoursesPage request, Pageable pageable);
 
 	CourseResponse updateCourse(Long id, CourseRequest request);
 
