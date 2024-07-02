8dcb1392a1c87c9aa5a1496930567c8441d7bb2d
Jacobs Agyei <agyeijacobs@outlook.com>
2024-07-02 09:00:27 +0000

    refactor(service): update create new user method

--- src/main/java/dev/aries/sagehub/service/userservice/UserServiceImpl.java
+++ src/main/java/dev/aries/sagehub/service/userservice/UserServiceImpl.java
@@ -22,10 +22,10 @@ import dev.aries.sagehub.repository.EmergencyContactRepository;
 import dev.aries.sagehub.repository.StaffRepository;
 import dev.aries.sagehub.repository.StudentRepository;
 import dev.aries.sagehub.repository.UserRepository;
+import dev.aries.sagehub.strategy.UpdateStrategy;
 import dev.aries.sagehub.util.Checks;
 import dev.aries.sagehub.util.Generators;
 import dev.aries.sagehub.util.GlobalUtil;
-import dev.aries.sagehub.strategy.UpdateStrategy;
 import dev.aries.sagehub.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -40,13 +40,14 @@ import static dev.aries.sagehub.constant.ExceptionConstants.NOT_FOUND;
  * UserServiceImpl is a service class that implements the UserService interface.
  * It provides methods for managing users, including changing passwords,
  * adding faculty members, and managing contact information.
- * @author Jacobs A
+ * @author Jacobs Agyei
  */
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService {
 
+	private static final String ROLE = "Role";
 	private final UserRepository userRepository;
 	private final StudentRepository studentRepository;
 	private final StaffRepository staffRepository;
@@ -59,22 +60,21 @@ public class UserServiceImpl implements UserService {
 	private final ContactInfoMapper contactInfoMapper;
 	private final EmergencyContactMapper emergencyContactMapper;
 	private final EmergencyContactRepository emergencyContactRepository;
-	private static final String ROLE = "Role";
 
 	/**
 	 * Changes the password of a user.
-	 * @param id the ID of the user.
+	 * @param id      the ID of the user.
 	 * @param request the request containing the old and new passwords.
 	 * @return a BasicUserResponse containing the updated user information.
 	 */
 	@Override
 	public BasicUserResponse changePassword(Long id, PasswordChangeRequest request) {
 		this.checks.isCurrentlyLoggedInUser(id);
-		if (!this.checks.isPasswordValid(request.oldPassword())) {
+		User user = this.userUtil.getUser(id);
+		if (!this.checks.isPasswordEqual(user, request.oldPassword())) {
 			log.info("INFO - Current password is incorrect");
 			throw new IllegalArgumentException(INVALID_CURRENT_PASSWORD);
 		}
-		User user = this.userUtil.getUser(id);
 		user.setHashedPassword(this.passwordEncoder.encode(request.newPassword()));
 		this.userRepository.save(user);
 		Object userInfo = this.userUtil.getUserInfo(id);
@@ -84,7 +84,7 @@ public class UserServiceImpl implements UserService {
 	/**
 	 * Adds a new faculty member.
 	 * @param request the request containing the user information.
-	 * @param role the role of the new faculty member (either "STUDENT" or "STAFF").
+	 * @param role    the role of the new faculty member (either "STUDENT" or "STAFF").
 	 * @return a BasicUserResponse containing the new faculty member's information.
 	 */
 	@Override
@@ -119,7 +119,7 @@ public class UserServiceImpl implements UserService {
 
 	/**
 	 * Updates the contact information of a user.
-	 * @param id the ID of the user.
+	 * @param id      the ID of the user.
 	 * @param request the request containing the new contact information.
 	 * @return a ContactInfoResponse containing the updated contact information.
 	 */
@@ -128,7 +128,7 @@ public class UserServiceImpl implements UserService {
 
 		this.checks.isAdminOrLoggedIn(id);
 		ContactInfo contactInfo = this.userUtil.loadContactInfo(this.userUtil.getUserInfo(id));
-		UpdateStrategy strategy = globalUtil.checkStrategy("updateContactInfo");
+		UpdateStrategy strategy = this.globalUtil.checkStrategy("updateContactInfo");
 		contactInfo = (ContactInfo) strategy.update(contactInfo, request);
 		this.contactInfoRepository.save(contactInfo);
 		log.info("INFO - Contact info with ID:{} updated", contactInfo.getId());
@@ -147,9 +147,10 @@ public class UserServiceImpl implements UserService {
 				this.userUtil.getUserInfo(id));
 		return this.emergencyContactMapper.toEmergencyContactResponse(emergencyContact);
 	}
+
 	/**
 	 * Updates the emergency contact information of a user.
-	 * @param id the ID of the user.
+	 * @param id      the ID of the user.
 	 * @param request the request containing the new emergency contact information.
 	 * @return a EmergencyContactResponse containing the updated emergency contact information.
 	 */
@@ -158,7 +159,7 @@ public class UserServiceImpl implements UserService {
 		this.checks.isAdminOrLoggedIn(id);
 		EmergencyContact emergencyContact = this.userUtil.loadEmergencyContact(
 				this.userUtil.getUserInfo(id));
-		UpdateStrategy strategy = globalUtil.checkStrategy("updateEmergencyContact");
+		UpdateStrategy strategy = this.globalUtil.checkStrategy("updateEmergencyContact");
 		emergencyContact = (EmergencyContact) strategy.update(emergencyContact, request);
 		this.emergencyContactRepository.save(emergencyContact);
 		log.info("INFO - Emergency contact with ID{} updated", emergencyContact.getId());
@@ -168,8 +169,7 @@ public class UserServiceImpl implements UserService {
 	/**
 	 * This method builds a BasicInfo object using the provided builder and request.
 	 * It sets the first name, middle name, last name, gender, and date of birth from the request.
-	 *
-	 * @param <T> a type that extends BasicInfo.BasicInfoBuilder.
+	 * @param <T>     a type that extends BasicInfo.BasicInfoBuilder.
 	 * @param builder the builder used to construct the BasicInfo object.
 	 * @param request the request containing the user information.
 	 * @return a BasicInfo object built with the provided builder and request.
@@ -179,12 +179,12 @@ public class UserServiceImpl implements UserService {
 		String firstname = request.firstname();
 		String lastname = request.lastname();
 		return builder.firstName(firstname)
-			.middleName(request.middleName())
-			.lastName(lastname)
-			.gender(Gender.valueOf(request.gender()))
-			.dateOfBirth(request.dateOfBirth())
-			.profilePictureUrl(request.profilePicture())
-			.build();
+				.middleName(request.middleName())
+				.lastName(lastname)
+				.gender(Gender.valueOf(request.gender()))
+				.dateOfBirth(request.dateOfBirth())
+				.profilePictureUrl(request.profilePicture())
+				.build();
 	}
 
 	/**
@@ -196,13 +196,14 @@ public class UserServiceImpl implements UserService {
 	 * user. Finally, it uses the buildBasicInfo method to set the first name, middle
 	 * name, last name, gender, and date of birth from the request.
 	 * @param request the request containing the user information.
-	 * @param role the role of the new user (either "STUDENT" or "STAFF").
+	 * @param role    the role of the new user (either "STUDENT" or "STAFF").
 	 * @return a BasicInfo object built with the provided builder and request.
 	 * @throws IllegalArgumentException if the role is not "STUDENT" or "STAFF".
 	 */
 	private BasicInfo buildInfo(AddUserRequest request, String role) {
-		User user = this.userUtil.createNewUser(
-				request.firstname(), request.lastname(), RoleEnum.valueOf(role));
+		String username = this.generators.generateUsername(request.firstname(), request.lastname());
+		String password = this.generators.generatePassword();
+		User user = this.userUtil.createNewUser(username, password, RoleEnum.valueOf(role));
 		Long id = addContactInfo(request.contactInfo());
 		ContactInfo contactInfo = this.contactInfoRepository.findById(id)
 				.orElseThrow();
@@ -220,6 +221,7 @@ public class UserServiceImpl implements UserService {
 					.user(user);
 			default -> throw new IllegalArgumentException(String.format(NOT_FOUND, ROLE));
 		};
+		log.info("INFO - New user added with username: {} and password: {}", username, password);
 		return buildBasicInfo(builder, request);
 	}
 
