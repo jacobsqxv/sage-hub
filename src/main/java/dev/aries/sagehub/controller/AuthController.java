30b5e9277e01225a07bad179d8ef6ffe4391189f
Jacobs Agyei <agyeijacobs@outlook.com>
2024-07-02 12:02:32 +0000

    refactor: add password reset endpoint

--- src/main/java/dev/aries/sagehub/controller/AuthController.java
+++ src/main/java/dev/aries/sagehub/controller/AuthController.java
@@ -1,12 +1,16 @@
 package dev.aries.sagehub.controller;
 
 import dev.aries.sagehub.dto.request.AuthRequest;
+import dev.aries.sagehub.dto.request.ResetPassword;
+import dev.aries.sagehub.dto.request.ResetPasswordRequest;
 import dev.aries.sagehub.dto.response.AuthResponse;
+import dev.aries.sagehub.dto.response.GenericResponse;
 import dev.aries.sagehub.service.authservice.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +28,14 @@ public class AuthController {
 		return ResponseEntity.ok(this.authService.authenticateUser(request));
 	}
 
+	@PostMapping("/reset-password")
+	public ResponseEntity<GenericResponse> resetPasswordRequest(@RequestBody @Valid ResetPasswordRequest request) {
+		return ResponseEntity.ok(this.authService.resetPasswordRequest(request));
+	}
+
+	@PostMapping("/reset-password/{token}")
+	public ResponseEntity<GenericResponse> resetPassword(@PathVariable String token,
+						@RequestBody @Valid ResetPassword request) {
+		return ResponseEntity.ok(this.authService.resetPassword(token, request));
+	}
 }
--- src/main/java/dev/aries/sagehub/service/authservice/AuthServiceImpl.java
+++ src/main/java/dev/aries/sagehub/service/authservice/AuthServiceImpl.java
@@ -4,22 +4,34 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import dev.aries.sagehub.constant.ExceptionConstants;
+import dev.aries.sagehub.constant.ResponseMessage;
 import dev.aries.sagehub.dto.request.AuthRequest;
+import dev.aries.sagehub.dto.request.ResetPassword;
+import dev.aries.sagehub.dto.request.ResetPasswordRequest;
 import dev.aries.sagehub.dto.response.AuthResponse;
 import dev.aries.sagehub.dto.response.BasicUserResponse;
+import dev.aries.sagehub.dto.response.GenericResponse;
+import dev.aries.sagehub.enums.TokenType;
+import dev.aries.sagehub.model.Token;
 import dev.aries.sagehub.model.User;
+import dev.aries.sagehub.repository.TokenRepository;
 import dev.aries.sagehub.repository.UserRepository;
 import dev.aries.sagehub.security.TokenService;
+import dev.aries.sagehub.util.Checks;
+import dev.aries.sagehub.util.Generators;
 import dev.aries.sagehub.util.GlobalUtil;
 import dev.aries.sagehub.util.UserUtil;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -37,7 +49,13 @@ public class AuthServiceImpl implements AuthService {
 	private final TokenService tokenService;
 	private final GlobalUtil globalUtil;
 	private final UserUtil userUtil;
+	private final Generators generator;
+	private final Checks checks;
 	private final UserRepository userRepository;
+	private final TokenRepository tokenRepository;
+	private final PasswordEncoder passwordEncoder;
+	private static final Integer STATUS_OK = HttpStatus.OK.value();
+
 	/**
 	 * Authenticates a user.
 	 * @param request the request containing the username and password.
@@ -89,6 +107,45 @@ public class AuthServiceImpl implements AuthService {
 		if (failedAttempts >= 5) {
 			user.setLockTime(LocalDateTime.now().plusMinutes(15));
 			user.setFailedLoginAttempts(0);
+		}
+	}
+
+	@Override
+	public GenericResponse resetPasswordRequest(ResetPasswordRequest request) {
+		User user = this.userUtil.getUser(request.username());
+		String value = this.generator.generateToken();
+		Token token = Token.builder()
+				.value(value)
+				.type(TokenType.RESET_PASSWORD)
+				.userId(user.getId())
+				.expiresAt(LocalDateTime.now().plusMinutes(15))
+				.build();
+		this.tokenRepository.save(token);
+		log.info("INFO - Reset password token: {}", value);
+		return new GenericResponse(STATUS_OK, String.format(ResponseMessage.EMAIL_SENT, "Password reset"));
+	}
+
+	@Override
+	public GenericResponse resetPassword(String token, ResetPassword request) {
+		log.info("INFO - User requesting password reset: {}", request.username());
+		User user = this.userUtil.getUser(request.username());
+		validateToken(token, user, TokenType.RESET_PASSWORD);
+		if (this.checks.isPasswordEqual(user, request.password())) {
+			throw new IllegalArgumentException(ExceptionConstants.INVALID_CURRENT_PASSWORD);
+		}
+		user.setHashedPassword(this.passwordEncoder.encode(request.password()));
+		this.userRepository.save(user);
+		return new GenericResponse(STATUS_OK, ResponseMessage.PASSWORD_RESET_SUCCESS);
+	}
+
+	private void validateToken(String value, User user, TokenType type) {
+		Long userId = user.getId();
+		Token token = this.tokenRepository.findByValueAndUserIdAndType(value, userId, type);
+		if (token == null) {
+			throw new EntityNotFoundException(String.format(ExceptionConstants.NOT_FOUND, "Token"));
+		}
+		if (token.getExpiresAt().isBefore(LocalDateTime.now())) {
+			throw new IllegalArgumentException(ExceptionConstants.EXPIRED_TOKEN);
 		}
 	}
 }
