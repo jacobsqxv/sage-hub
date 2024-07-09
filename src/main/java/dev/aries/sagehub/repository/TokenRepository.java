b7a846d1540918e832c4b273ba433536203c5ae2
Jacobs Agyei <agyeijacobs@outlook.com>
2024-07-09 19:10:00 +0000

    feat(repository): add application repository

--- src/main/java/dev/aries/sagehub/repository/TokenRepository.java
+++ src/main/java/dev/aries/sagehub/repository/TokenRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
 	Token findByValueAndUserIdAndType(String value, Long userId, TokenType type);
+
+	Token findByValueAndType(String value, TokenType tokenType);
 }
