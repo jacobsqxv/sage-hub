12856004ff3b29e484cd0c8e5a908dfe2d919612
Jacobs Agyei <agyeijacobs@outlook.com>
2024-07-20 16:09:18 +0000

    style(repository): renamed applicationrepository

--- src/main/java/dev/aries/sagehub/repository/ApplicantRepository.java
+++ src/main/java/dev/aries/sagehub/repository/ApplicantRepository.java
@@ -6,7 +6,8 @@ import dev.aries.sagehub.model.Applicant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApplicationRepository extends JpaRepository<Applicant, Long> {
+public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
 	Optional<Applicant> findByUserId(Long id);
 
+	boolean existsByIdAndUserId(Long applicationId, Long userId);
 }
