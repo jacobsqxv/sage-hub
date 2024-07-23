f167c40682ba076ecc16094ed2d0d2ba28504ffb
Jacobs Agyei <agyeijacobs@outlook.com>
2024-07-23 16:32:15 +0000

    feat(config): update auditor and add rate limiting to aspect class

--- src/main/java/dev/aries/sagehub/config/SecurityAuditorAware.java
+++ src/main/java/dev/aries/sagehub/config/SecurityAuditorAware.java
@@ -2,6 +2,7 @@ package dev.aries.sagehub.config;
 
 import java.util.Optional;
 
+import dev.aries.sagehub.constant.ExceptionConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 
@@ -9,6 +10,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
@@ -20,19 +22,38 @@ public class SecurityAuditorAware implements AuditorAware<String> {
 	public Optional<String> getCurrentAuditor() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		String requestId = MDC.get("requestId");
-		if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+		if (authentication == null || !authentication.isAuthenticated()
+				|| authentication instanceof AnonymousAuthenticationToken) {
 			if (authentication == null) {
 				log.info("Auditor: SYSTEM");
 				return Optional.of("SYSTEM");
 			}
+
 			WebAuthenticationDetails request = (WebAuthenticationDetails) authentication.getDetails();
 			if (request != null) {
-				log.info("Auditor (Unauthenticated): {}, Request ID: {}", request.getRemoteAddress(), requestId);
-				return Optional.of(request.getRemoteAddress());
+				try {
+					String clientId = getClientId(authentication);
+					log.info("Auditor: {}", clientId);
+					return Optional.of(clientId);
+				}
+				catch (IllegalArgumentException ex) {
+					log.info("Auditor (No auth): {}, Request ID: {}",
+							request.getRemoteAddress(), requestId);
+					return Optional.of(request.getRemoteAddress());
+				}
 			}
 		}
 
-		log.info("Auditor: {}, Request ID: {}", authentication.getName(), requestId);
-		return Optional.of((authentication.getName()));
+		log.info("Auditor: {}, Request ID: {}", getClientId(authentication), requestId);
+		return Optional.of(getClientId(authentication));
+	}
+
+	public String getClientId(Authentication authentication) {
+		if (authentication.getPrincipal() instanceof Jwt jwt) {
+			return jwt.getClaim("clientId");
+		}
+		else {
+			throw new IllegalArgumentException(ExceptionConstants.INVALID_CREDENTIALS);
+		}
 	}
 }
