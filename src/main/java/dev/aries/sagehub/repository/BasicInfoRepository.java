b8563c98c3264405bdcd3e53f031dd6b327476cd
Jacobs Agyei <agyeijacobs@outlook.com>
2024-07-24 16:20:05 +0000

    refactor(repository): update findById query

--- src/main/java/dev/aries/sagehub/repository/BasicInfoRepository.java
+++ src/main/java/dev/aries/sagehub/repository/BasicInfoRepository.java
@@ -1,8 +1,11 @@
 package dev.aries.sagehub.repository;
 
+import java.util.Optional;
+
 import dev.aries.sagehub.model.BasicInfo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BasicInfoRepository extends JpaRepository<BasicInfo, Long> {
+	Optional<BasicInfo> findByUserId(Long userId);
 }
--- src/main/java/dev/aries/sagehub/repository/EmergencyContactRepository.java
+++ src/main/java/dev/aries/sagehub/repository/EmergencyContactRepository.java
@@ -1,9 +1,11 @@
 package dev.aries.sagehub.repository;
 
+import java.util.Optional;
+
 import dev.aries.sagehub.model.EmergencyContact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmergencyContactRepository extends JpaRepository<EmergencyContact, Long> {
-
+	Optional<EmergencyContact> findByUserId(Long userId);
 }
