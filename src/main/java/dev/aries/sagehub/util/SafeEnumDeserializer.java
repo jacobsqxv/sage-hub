eaa7d8081c12cfbfad060c26de5f0ffb598ea30e
Jacobs Agyei <agyeijacobs@outlook.com>
2024-08-04 16:36:24 +0000

    feat(util): add deserializer for enums in requests

---
+++ src/main/java/dev/aries/sagehub/util/SafeEnumDeserializer.java
@@ -0,0 +1,26 @@
+package dev.aries.sagehub.util;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import dev.aries.sagehub.constant.ExceptionConstants;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SafeEnumDeserializer<E extends Enum<E>> extends JsonDeserializer<E> {
+	private Class<E> enumClass;
+
+	@Override
+	public E deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+		String value = jsonParser.getText();
+		try {
+			return Enum.valueOf(enumClass, value.toUpperCase());
+		}
+		catch (IllegalArgumentException ex) {
+			throw new IllegalArgumentException(String.format(ExceptionConstants.ENUM_VALUE_INVALID,
+					value));
+		}
+	}
+}
