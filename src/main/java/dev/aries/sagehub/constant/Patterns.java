f99a3810bbcaa2029a252b3da6f479befc10ed7f
Jacobs Agyei <agyeijacobs@outlook.com>
2024-08-06 13:43:41 +0000

    refactor: update patterns and add attribute for grade

--- src/main/java/dev/aries/sagehub/constant/Patterns.java
+++ src/main/java/dev/aries/sagehub/constant/Patterns.java
@@ -13,15 +13,13 @@ public final class Patterns {
 	 * symbol, followed by domain parts separated by dots. The domain part allows 2 to 4
 	 * alphanumeric characters.
 	 */
-	public static final String EMAIL = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+"
-			+ "@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?"
-			+ "(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+	public static final String EMAIL = "^[A-Za-z0-9._%+-]{3,64}@[A-Za-z0-9-]+(\\.[A-Za-z]{2,4}){1,2}$";
 
 	/**
 	 * Regular expression for validating IDs.
 	 * It allows for IDs that are 7 to 10 digits long, catering to a range of identification numbers.
 	 */
-	public static final String ID = "^[0-9]{7,10}$";
+	public static final String ID = "^\\d{7,10}$";
 
 	/**
 	 * Regular expression for validating names.
@@ -29,37 +27,42 @@ public final class Patterns {
 	 * apostrophes, commas, periods, spaces, and hyphens.
 	 * This pattern supports first and last names, including compound names.
 	 */
-	public static final String NAME = "^([a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*){3,}$";
+	public static final String NAME = "^([A-Za-z]{4,20}(?:-[A-Za-z]{4,20})?\\s){0,5}$";
 
 	/**
 	 * Regular expression for validating location names.
 	 * It allows for location names with alphabetic characters, including spaces, commas, periods, and hyphens.
 	 */
-	public static final String LOCATION = "^[a-zA-Z]+([',. -]*[a-zA-Z]+)*{4,}$";
+	public static final String LOCATION = "^([A-Za-z]{4,20}(?:-[A-Za-z]{4,20})?\\s){0,3}$";
 
 	/**
 	 * Regular expression for validating street addresses. It allows for street addresses
 	 * with alphanumeric characters, including spaces, commas, periods, and hyphens.
 	 */
-	public static final String STREET = "^(?:(\\d+){3,}(?:,)?\\s+)?[A-Za-z{5,}\\s]+$";
+	public static final String STREET = "^(?:(\\d){3,7},?\\s)?([A-Za-z]{4,20}(?:-[A-Za-z]{4,20})?\\s){0,4}$";
 
 	/**
 	 * Regular expression for validating passwords.
 	 * It requires at least one digit, one lowercase letter, one uppercase letter, one special character,
 	 * and at least 8 characters in total without any spaces.
 	 */
-	public static final String PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+	public static final String PASSWORD = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=\\S+$).{8,30}$";
 
 	/**
 	 * Regular expression for validating postal addresses.
 	 * It supports alphanumeric characters, including spaces, commas, periods, and hyphens.
 	 */
-	public static final String POSTAL_ADDRESS = "^(PO|P\\.O)\\.\\s*Box\\s+(\\w+){4,}(?:,)?\\s+([\\w\\s]+){4,}$";
+	public static final String POSTAL_ADDRESS = "^(PO|P\\.O)\\.\\s*Box\\s\\w{4,20},?\\s+\\w{4,25}([- ]\\w{4,25})?$";
 
 	/**
 	 * Regular expression for validating JWT tokens.
 	 */
 	public static final String JWT_PATTERN = "^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_.+/=]*$";
+
+	/**
+	 * Regular expression for validating grades.
+	 */
+	public static final String GRADE = "^[A-F][+-]?$";
 
 	private Patterns() {
 		throw new IllegalStateException(ExceptionConstants.UTILITY_CLASS);
